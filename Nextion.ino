7eb2e8bff42b3c9f9da4b63fa05b6d9e4c2731c0
Szép Norbert <szepnorbee@gmail.com>
2017-08-26 09:57:16 +0200

    First upload
    Implemented the communication with the Nextion display.

---
+++ Nextion.ino
@@ -0,0 +1,179 @@
+void updMain() {
+  int a =0;
+  int b =0;
+  a = map(fTemp,1,1200,1,100);
+  b = map(vTemp,1,128,1,100);
+  myNextion.setComponentValue("fVar", a);
+  myNextion.setComponentValue("vVar", b);
+  myNextion.setComponentText("fTemp", String(fTemp));
+  myNextion.setComponentText("vTemp", String(vTemp));
+  if (stdby) {
+    myNextion.sendCommand("run.val=1");
+  } else {
+    myNextion.sendCommand("run.val=0");
+  }
+  Serial.println("DATA SENDING...");
+}
+
+void updTimers1() {
+  myNextion.setComponentText("fStartNr", String(fStart));
+  myNextion.setComponentText("fStopNr", String(fStop));
+  Serial.println("DATA SENDING...");
+}
+
+void updTimers2() {
+  myNextion.setComponentText("tStartNr", String(tStart));
+  myNextion.setComponentText("tStopNr", String(tStop));
+  Serial.println("DATA SENDING...");
+}
+
+void updSettings() {
+  myNextion.setComponentText("hiszt", String(hiszter));
+  myNextion.setComponentText("levK", String(fanDelay));
+  //delay(50);
+  if (thermostat==true){
+    myNextion.setComponentText("termState", "BE");
+  } else {
+    myNextion.setComponentText("termState", "KI");
+  }
+  if (debug==true){
+    myNextion.setComponentText("debugState", "BE");
+  } else {
+    myNextion.setComponentText("debugState", "KI");
+  }
+  Serial.println("DATA SENDING...");
+}
+
+void dataIn(String message) {
+  Serial.println("INCOMING");
+// MENU NAVIGATION //////
+  if (message=="65 0 3 0 ff ff ff") {
+    myNextion.sendCommand("page Settings");
+    updSettings();
+  }
+  if (message=="65 3 1 0 ff ff ff" || message=="65 1 b 0 ff ff ff" || message=="65 2 b 0 ff ff ff" || message=="65 4 1 0 ff ff ff") {
+    myNextion.sendCommand("page Main");
+    updMain();
+  }
+  if (message=="65 0 2 0 ff ff ff" || message=="65 2 9 0 ff ff ff") {
+    myNextion.sendCommand("page Timers1");
+    updTimers1();
+  }
+  if (message=="65 1 9 0 ff ff ff") {
+    myNextion.sendCommand("page Timers2");
+    updTimers2();
+  }
+  if (message=="65 0 1 0 ff ff ff") {
+    myNextion.sendCommand("page Manual");
+  }
+
+/////////////////////////
+// SET VALUES ///////////
+
+/// Fütés start ///
+  if (message=="65 1 4 0 ff ff ff") {
+    fStart++;
+    myNextion.setComponentText("fStartNr", String(fStart));
+  }
+  if (message=="65 1 3 0 ff ff ff") {
+    fStart+=5;
+    myNextion.setComponentText("fStartNr", String(fStart));
+  }
+  if (message=="65 1 1 0 ff ff ff") {
+    fStart--;
+    myNextion.setComponentText("fStartNr", String(fStart));
+  }
+  if (message=="65 1 2 0 ff ff ff") {
+    fStart-=5;
+    myNextion.setComponentText("fStartNr", String(fStart));
+  }
+
+/// Fütés stop ///
+  if (message=="65 1 6 0 ff ff ff") {
+    fStop++;
+    myNextion.setComponentText("fStopNr", String(fStop));
+  }
+  if (message=="65 1 7 0 ff ff ff") {
+    fStop+=5;
+    myNextion.setComponentText("fStopNr", String(fStop));
+  }
+  if (message=="65 1 5 0 ff ff ff") {
+    fStop--;
+    myNextion.setComponentText("fStopNr", String(fStop));
+  }
+  if (message=="65 1 8 0 ff ff ff") {
+    fStop-=5;
+    myNextion.setComponentText("fStopNr", String(fStop));
+  }
+
+/// Tartás start ///
+  if (message=="65 2 1 0 ff ff ff") {
+    tStart++;
+    myNextion.setComponentText("tStartNr", String(tStart));
+  }
+  if (message=="65 2 7 0 ff ff ff") {
+    tStart+=5;
+    myNextion.setComponentText("tStartNr", String(tStart));
+  }
+  if (message=="65 2 4 0 ff ff ff") {
+    tStart--;
+    myNextion.setComponentText("tStartNr", String(tStart));
+  }
+  if (message=="65 2 6 0 ff ff ff") {
+    tStart-=5;
+    myNextion.setComponentText("tStartNr", String(tStart));
+  }
+
+/// Tartás stop ///
+  if (message=="65 2 8 0 ff ff ff") {
+    tStop++;
+    myNextion.setComponentText("tStopNr", String(tStop));
+  }
+  if (message=="65 2 5 0 ff ff ff") {
+    tStop+=5;
+    myNextion.setComponentText("tStopNr", String(tStop));
+  }
+  if (message=="65 2 2 0 ff ff ff") {
+    tStop--;
+    myNextion.setComponentText("tStopNr", String(tStop));
+  }
+  if (message=="65 2 3 0 ff ff ff") {
+    tStop-=5;
+    myNextion.setComponentText("tStopNr", String(tStop));
+  }
+/// Settings page ///
+  if (message=="65 3 3 0 ff ff ff") {
+    if (thermostat==true) {
+      thermostat=false;
+    } else {
+      thermostat=true;
+    }
+  updSettings();
+  }
+  
+  if (message=="65 3 2 0 ff ff ff") {
+    if (debug==true) {
+      debug=false;
+    } else {
+      debug=true;
+    }
+  updSettings();
+  }
+
+  if (message=="65 3 4 0 ff ff ff") {
+    hiszter++;
+    myNextion.setComponentText("hiszt", String(hiszter));
+  }
+  if (message=="65 3 7 0 ff ff ff") {
+    hiszter--;
+    myNextion.setComponentText("hiszt", String(hiszter));
+  }
+  if (message=="65 3 5 0 ff ff ff") {
+    fanDelay++;
+    myNextion.setComponentText("levK", String(fanDelay));
+  }
+  if (message=="65 3 6 0 ff ff ff") {
+    fanDelay--;
+    myNextion.setComponentText("levK", String(fanDelay));
+  }
+}
---
+++ kazan_vezerlo_nextion.ino
@@ -0,0 +1,61 @@
+#include <SoftwareSerial.h>
+#include <Nextion.h>
+
+byte fStart = 5;
+byte fStop = 5;
+byte tStart = 5;
+byte tStop = 5;
+byte hiszter = 3;
+byte fanDelay = 10;
+boolean thermostat = true;
+boolean debug = false;
+float fTemp = 850.7;
+float vTemp = 42.5;
+
+boolean mainMenu = true;
+
+long previousMillis = 0; 
+long interval = 1000; 
+boolean stdby = true;
+
+SoftwareSerial nextion(4, 5);// Nextion TX to pin 2 and RX to pin 3 of Arduino
+
+Nextion myNextion(nextion, 115200); 
+
+void setup() {
+  Serial.begin(115200);
+  myNextion.init();
+  Serial.println("START...");
+  myNextion.setComponentText("fTemp", "PROGRAM");
+  myNextion.setComponentText("vTemp", "START");
+  delay(1000);
+  updMain();
+}
+
+void loop() {
+// NEXTION UPDATE //
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis > interval) {
+    previousMillis = currentMillis; 
+    if(mainMenu==true) { 
+   /////Stdby/////
+   if(stdby) {
+    stdby=false;
+   } else {
+    stdby=true;
+   }
+   ///////////////
+    updMain();
+    }
+  }
+////////////////////
+
+  String message = myNextion.listen();
+
+  if (message != "") { // if a message is received...
+    Serial.print("MESSAGE: ");
+    Serial.println(message); //...print it out
+    dataIn(message);
+  }  
+}
+
